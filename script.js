2de7bbb4a8c79f3bc6fd4c401a99ddd51b0da892
HigorGoltara <102702183+HigorGoltara@users.noreply.github.com>
2022-05-07 10:48:23 -0300

    Add files via upload
    Pequeno relogio digital feito usando um pouco do JavaScript como metodo de prática

---
+++ script.js
@@ -0,0 +1,25 @@
+setInterval (function() {
+    const clock = document.querySelector(".display");
+let time = new Date();
+let seconds = time.getSeconds();
+let minutes = time.getMinutes();
+let hours = time.getHours();
+let day = "AM";
+let night = "PM";
+if (hours > 12) {
+   hours = hours - 12; 
+}
+if (hours == 0) {
+    hours = 12;
+}
+if (seconds < 10) {
+    seconds = "0" + seconds;
+}
+if (minutes < 10) {
+    minutes = "0" + minutes;
+}
+if (hours < 10) {
+    hours = "0" + hours;
+}
+clock.textContent = hours + ":" + minutes + ":" + seconds + " " + day;
+})
